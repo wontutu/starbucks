5d8bef67f6c7fe31e2de3538fb87962fd1a29bc0
wontutu <cwt9999@gmail.com>
2022-09-22 21:20:05 +0900

    start project

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+
+searchEl.addEventListener("mouseenter", function () {
+  searchInputEl.focus();
+});
+searchEl.addEventListener("mouseleave", function () {
+  searchInputEl.blur();
+});
+
+searchInputEl.addEventListener("focus", function () {
+  searchEl.classList.add("focused");
+  searchInputEl.setAttribute("placeholder", "통합검색");
+});
+searchInputEl.addEventListener("blur", function () {
+  searchEl.classList.remove("focused");
+  searchInputEl.setAttribute("placeholder", "");
+});
+
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    console.log("scroll");
+    if (window.scrollY > 500) {
+      // gsap.to(요소, 지속시간, 옵션)
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
+// _.throttle(함수, 시간)
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
+});
